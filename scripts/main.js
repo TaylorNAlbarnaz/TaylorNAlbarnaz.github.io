71785f9a4aba44be26c51aa7026915eb77778e4d
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-11-24 12:34:21 -0300

    Melhorias no design e adição de Jquery e Swiper

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,13 @@ const navMenu = $('#nav-menu');
 const navToggle = $('#nav-toggle');
 const navClose = $('#nav-close');
 
-// Opens Menu
+// Opens/Closes Menu
 if (navToggle) {
     navToggle.click(()=> {
         navMenu.addClass('show-menu');
     })
 }
 
-// Closes Menu
 if (navClose) {
     navClose.click(()=> {
         navMenu.removeClass('show-menu');
@@ -17,10 +16,53 @@ if (navClose) {
 }
 
 // Closes menu from Nav Items
-const navLink = document.querySelectorAll('.nav__link');
+const navLink = $('.nav__link');
 
-function linkAction() {
-    navMenu.removeClass('show-menu');
+navLink.each(function () {
+    $(this).click(function() {
+        navMenu.removeClass('show-menu');
+    });
+});
+
+
+// Open/Closes SKills Section
+const skillsContent = $('.skills__content');
+const skillsHeader = $('.skills__header');
+
+console.log('1');
+
+function toggleSkills(e) {
+    const skillsParent = $(e.currentTarget).parent();
+
+    skillsContent.each(function() {
+        $(this).removeClass('skills__open');
+        $(this).addClass('skills__close');
+    })
+    
+    if (skillsParent.hasClass('skills__close')) {
+        skillsParent.removeClass('skills__close')
+        skillsParent.addClass('skills__open');
+    }
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction));
+skillsHeader.each(function() {
+    console.log('8');
+
+    $(this).on("click", function(e){
+        console.log('9');
+        toggleSkills(e);
+    });
+})
+
+let swiper = new Swiper(".portfolio__container", {
+    cssMode: true,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+});
