88c1e8ab17027b726a55e61ea5360b9625de4b4a
TaylorNAlbarnaz <veloxgods@gmail.com>
2022-11-26 14:18:00 -0300

    Layout refeito, HeaderBar pronto

--- scripts/main.js
+++ scripts/main.js
@@ -1,68 +1,29 @@
-const navMenu = $('#nav-menu');
-const navToggle = $('#nav-toggle');
-const navClose = $('#nav-close');
+// Open Mobile Menu
+const openMenu = document.getElementById('open-menu');
 
-// Opens/Closes Menu
-if (navToggle) {
-    navToggle.click(()=> {
-        navMenu.addClass('show-menu');
-    })
-}
-
-if (navClose) {
-    navClose.click(()=> {
-        navMenu.removeClass('show-menu');
-    })
-}
-
-// Closes menu from Nav Items
-const navLink = $('.nav__link');
-
-navLink.each(function () {
-    $(this).click(function() {
-        navMenu.removeClass('show-menu');
-    });
+openMenu.addEventListener('click', ()=> {
+    var menuMobile = document.getElementById('nav__buttons-mobile');
+    if (menuMobile.classList.contains('show')) {
+        menuMobile.classList.remove('show');
+    } else {
+        menuMobile.classList.add('show');
+    }
 });
 
+// Change website theme
+const darkLightButton = document.getElementById('dark-light-btn');
 
-// Open/Closes SKills Section
-const skillsContent = $('.skills__content');
-const skillsHeader = $('.skills__header');
-
-console.log('1');
-
-function toggleSkills(e) {
-    const skillsParent = $(e.currentTarget).parent();
-
-    skillsContent.each(function() {
-        $(this).removeClass('skills__open');
-        $(this).addClass('skills__close');
-    })
-    
-    if (skillsParent.hasClass('skills__close')) {
-        skillsParent.removeClass('skills__close')
-        skillsParent.addClass('skills__open');
+function ChangeTheme(e) {
+    if (e.classList.contains('dark-mode')) {
+        e.classList.remove('dark-mode');
+        e.classList.add('light-mode');
+    } else {
+        e.classList.remove('light-mode');
+        e.classList.add('dark-mode');
     }
 }
 
-skillsHeader.each(function() {
-    console.log('8');
-
-    $(this).on("click", function(e){
-        console.log('9');
-        toggleSkills(e);
-    });
-})
-
-let swiper = new Swiper(".portfolio__container", {
-    cssMode: true,
-    loop: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    },
+darkLightButton.addEventListener('click', ()=> {
+    ChangeTheme(document.getElementById('nav__buttons-mobile'));
+    ChangeTheme(document.getElementsByClassName('nav__bar')[0]);
 });
