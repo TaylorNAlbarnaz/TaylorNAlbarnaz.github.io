ea3627fb5a9828133da26b7e4ede820eedbe636e
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-01-31 23:22:15 -0300

    Roteamento

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function About() {
   return (
     <>
       <span>Sobre</span>
+      <br />
+      <Link href="/">Página Inicial</Link>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <span>Olá mundo</span>
+      <br />
+      <Link href="/about">Sobre</Link>
     </>
   )
 }
