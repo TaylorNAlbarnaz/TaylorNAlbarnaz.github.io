1e4e6b6a4b3c397139966a8f72573c86dd31cc83
TaylorNAlbarnaz <veloxgods@gmail.com>
2023-02-07 23:26:17 -0300

    Modificação

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -3,9 +3,9 @@ import Link from "next/link";
 export default function About() {
   return (
     <>
-      <span>Sobre</span>
+      <span>Página Sobre</span>
       <br />
-      <Link href="/">Página Inicial</Link>
+      <Link href="/">Inicial</Link>
     </>
   );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <>
-      <span>Olá mundo</span>
+      <span>Página Inicial</span>
       <br />
       <Link href="/about">Sobre</Link>
     </>
